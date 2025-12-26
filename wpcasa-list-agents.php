51e7ee884cb85496b253ccaed892bc62bb5ed981
Sandro <sandro@openstream.ch>
2025-12-26 21:26:43 +0100

    new repository file structure

--- wpcasa-list-agents.php
+++ wpcasa-list-agents.php
@@ -7,13 +7,10 @@ Version: 1.0.0
 Author: WPSight
 Author URI: http://wpsight.com
 Requires at least: 4.0
+Requires Plugins:  wpcasa
 Tested up to: 4.3.1
 Text Domain: wpcasa-list-agents
 Domain Path: /languages
-
-	Copyright: 2015 Simon Rimkus
-	License: GNU General Public License v2.0 or later
-	License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 // Exit if accessed directly
